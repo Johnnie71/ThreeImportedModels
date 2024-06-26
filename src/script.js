00d5a089c2738e3e4832ae27cfa4474596684562
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-06-26 14:46:38 -0400

    add: using draco loader, compresses the size

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
@@ -18,10 +19,14 @@ const scene = new THREE.Scene()
 /**
  * Models
  */
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/')
+
 const gtlfLoader = new GLTFLoader();
+gtlfLoader.setDRACOLoader(dracoLoader)
 
 gtlfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/Duck/glTF-Draco/Duck.gltf',
     (gtlf) => {
         scene.add(gtlf.scene)
     }
